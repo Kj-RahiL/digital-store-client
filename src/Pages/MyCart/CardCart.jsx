9edc6e39d73c758ab278d93c4f69755feaeefe30
Kj-RahiL <rahiilarham@gmail.com>
2023-10-21 02:19:49 +0600

    added delete to dynamically my cart

--- src/Pages/MyCart/CardCart.jsx
+++ src/Pages/MyCart/CardCart.jsx
@@ -1,6 +1,44 @@
 
-const CardCart = ({ cart }) => {
-    const { name, brandName, image, price } = cart
+import Swal from "sweetalert2";
+import { ImCross } from "react-icons/im";
+
+const CardCart = ({ cart, carts, setCarts }) => {
+    const {_id, name, brandName, image, price } = cart
+    
+
+    const handleDelete = _id => {
+        console.log('handle delete' , _id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:8000/myCart/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            const remaining = carts.filter(cart=> cart._id !== _id)
+                            setCarts(remaining)
+                        }
+                    })
+
+            }
+        })
+    }
+
     return (
         <tr className="text-white text-lg text-center ">
             <td>
@@ -12,7 +50,7 @@ const CardCart = ({ cart }) => {
             <td>{brandName}</td>
             <td className="bg-slate-500">{price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">delete</button>
+            <button onClick={() => handleDelete(_id)} className="btn btn-ghost text-slate-50"><ImCross></ImCross></button>
             </th>
         </tr>
     );
--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -1,29 +1,38 @@
 import { useLoaderData } from "react-router-dom";
 import CardCart from "./CardCart";
+import { useState } from "react";
 
 
 const MyCart = () => {
     const products = useLoaderData()
+
+    const [carts, setCarts] = useState(products)
     console.log(products)
     return (
 
         <div className="overflow-x-auto mx-5 lg:mx-20 card-body ">
             <table className="table rounded-lg">
                 {/* head */}
-                <thead className="bg-amber-600 text-white font-semibold text-xl text-center">
+                <thead className="bg-amber-800 text-white font-semibold text-xl text-center">
                     <tr>
                         <th>Image</th>
                         <th>Name</th>
                         <th>Brand</th>
-                        <th>Type</th>
                         <th>Price</th>
+                        <th>Remove</th>
 
                     </tr>
                 </thead>
                 <tbody className="bg-gray-400">
                     
                         {
-                            products.map((cart, index) => <CardCart key={index} cart={cart} ></CardCart>)
+                            carts.map((cart, index) => <CardCart 
+                            key={index}
+                            cart={cart}
+                            carts={carts}
+                            setCarts={setCarts}
+                            
+                            ></CardCart>)
                         }
                  
 
